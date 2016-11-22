cd0f49291ef179f65bf3bc8b36b17c4d89a96fea
emeraldpiggy <george@expert4.com.au>
2016-11-23 07:12:26 +1100

    add big sum

--- Hackerrank/BigSum.cpp
+++ Hackerrank/BigSum.cpp
@@ -1,9 +1,22 @@
 #include "stdafx.h"
 #include "BigSum.h"
+#include <ostream>
+#include <iostream>
+#include <vector>
 
 
 int BigSum()
 {
+	int n;
+	std::cin >> n;
+	std::vector<int> arr(n);
+	long long sum = 0;
+	for (int arr_i = 0;arr_i < n;arr_i++) {
+		std::cin >> arr[arr_i];
+		sum += arr[arr_i];
+	}
+
+	std::cout << sum << std::endl;
 	return 0;
 }
 
